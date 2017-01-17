4151808e259487c2b801ac1d8369d21c1e6dee9d
Frank Theile <ftheile@grundfos.com>
2017-01-17 17:30:18 +0100

    AES: aes_gen_tables(): reduce stack usage by applying suggestion from https://github.com/ARMmbed/mbedtls/issues/744

--- library/aes.c
+++ library/aes.c
@@ -406,8 +406,8 @@ static int aes_init_done = 0;
 static void aes_gen_tables( void )
 {
     int i, x, y, z;
-    int pow[256];
-    int log[256];
+    unsigned char pow[256];
+    unsigned char log[256];
 
     /*
      * compute pow and log tables over GF(2^8)
